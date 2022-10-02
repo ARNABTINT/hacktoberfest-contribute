0c835011c891f1125842dc835f502e7ffb71c3f3
ARNABTINT <91847017+ARNABTINT@users.noreply.github.com>
2022-10-03 00:49:44 +0530

    Update Aniruddha775_Prime.c

--- Aniruddha775_Prime.c
+++ Aniruddha775_Prime.c
@@ -1,20 +1,25 @@
 #include <stdio.h> 
 
-int main() {
+int main()
+ {
   int n, i, c = 0;
   printf ("Enter any number ");
   scanf ("%d", &n);
-  for (i = 1; i <= n; i++) {
-      if (n % i == 0) {
+  for (i = 1; i <= n; i++)
+     {
+      if (n % i == 0)
+        {
          c++;
-      }
-  }
+        } 
+     }
 
-  if (c == 3) {
-  printf ("It is a Prime number");
-  }
-  else {
-  printf ("It is not a Prime number");
-  }
-  return 0;    
-}
+     if (c == 3)
+     {
+     printf ("It is a Prime number");
+     }
+     else
+     {
+      printf ("It is not a Prime number");
+      }
+      return 0;    
+ }
